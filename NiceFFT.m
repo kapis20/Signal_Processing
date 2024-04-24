88e927c9af767977e7cc5ab4533a0f2382b5ec06
kapis20 <kacper.filip.sikorski@gmail.com>
2024-04-24 22:49:48 +0100

    FFT function added, signal divided into segments and plots of FFT with conclusions made

---
+++ NiceFFT.m
@@ -0,0 +1,11 @@
+function [f,FT] = NiceFFT(data)
+% calculates the Fast Fourier Transform (FFT) of a given signal segment 
+% and returns the frequency vector 'f' in (radians per sample) 
+% andd the transformed signal FT
+N = length(data);
+FT = fftshift(fft(data));
+index = ceil(-N/2):floor(N/2)-1;
+f = 2*pi*index/N; %normalised w (radians per sample) 
+
+end
+
